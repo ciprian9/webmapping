bf3c26e4d957e2a3de0d78462895da51c7b8be6e
ciprian9 <c15341751@mydit.ie>
2018-10-31 16:53:12 +0000

    Second Commit

--- location/models.py
+++ location/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
 
@@ -12,3 +13,21 @@ class Location(models.Model):
     class Meta:
         ordering = ('created',)
 
+class WorldBorder(models.Model):
+    name = models.CharField(max_length=50)
+    area = models.IntegerField()   
+    pop2005 = models.IntegerField('Population 2005')    
+    fips = models.CharField('FIPS Code', max_length=2)    
+    iso2 = models.CharField('2 Digit ISO', max_length=2, default='')    
+    iso3 = models.CharField('3 Digit iso', max_length=3)
+    un = models.IntegerField('United Nations Code')
+    region = models.IntegerField('Region Code')
+    subregion = models.IntegerField('Sub-region code')
+    lon = models.FloatField()
+    lat = models.FloatField()
+
+
+    mpoly = models.MultiPolygonField()
+
+    def __str__(self):
+        return self.name
--- untitled7/settings.py
+++ untitled7/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '02=-2-w_@4gxj-7r0gxscx8j44grs%0+zot-x$o#me3njtgf%)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
     'location',
 ]
